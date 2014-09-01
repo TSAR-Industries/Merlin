02cc631f7fb839f561c12330cff90c26cbc7982e
RichoDemus <richodemus@gmail.com>
2014-09-01 12:36:43 +0200

    Initial commit

---
+++ backend/src/main/java/com/richo/merlin/backend/inject/BackendModule.java
@@ -0,0 +1,11 @@
+package com.richo.merlin.backend.inject;
+
+import com.google.inject.AbstractModule;
+
+public class BackendModule extends AbstractModule
+{
+	@Override
+	protected void configure()
+	{
+	}
+}
